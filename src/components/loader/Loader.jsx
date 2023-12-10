1743e23ed2352b9430fec69d1edbda61bb4d6bb0
Ozod <o.bukhorizoda@mail.ru>
2023-12-10 18:15:04 +0500

    fixed the loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -2,7 +2,7 @@ import styles from "./Loader.module.css";
 export const Loader = () => {
   return (
     <div className={styles.loaderContainer}>
-      <img className={styles.loaderGif} src="public/tictacgif.gif" alt="TicTacToe" />
+      <img className={styles.loaderGif} src="tictacgif.gif" alt="TicTacToe" />
       <h2 className={styles.loader_title}>Loading...</h2>
     </div>
   );
